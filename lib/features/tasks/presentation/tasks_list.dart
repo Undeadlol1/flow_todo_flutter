1d342c0e684aea1ec1e782b7c52547cbf6916166
Mikhail Palei <mikhail_palei@epam.com>
2022-06-05 19:00:28 +0500

    feat: :construction: working on TasksList

--- lib/features/tasks/presentation/tasks_list.dart
+++ lib/features/tasks/presentation/tasks_list.dart
@@ -1,10 +1,23 @@
+import 'tasks_list_item.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'cubit/tasks_cubit.dart';
 
 class TasksList extends StatelessWidget {
   const TasksList({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return BlocBuilder<TasksCubit, TasksState>(
+      builder: (context, state) {
+        return ListView.builder(
+          itemCount: state.tasks.length,
+          itemBuilder: (_, index) {
+            return TasksListItem(task: state.tasks[index]);
+          },
+        );
+      },
+    );
   }
 }
--- test/features/tasks/presentation/tasks_list_test.dart
+++ test/features/tasks/presentation/tasks_list_test.dart
@@ -1,29 +1,60 @@
+import 'package:flow_todo_flutter_2022/features/tasks/presentation/cubit/tasks_cubit.dart';
 import 'package:flow_todo_flutter_2022/features/tasks/presentation/tasks_list.dart';
+import 'package:flow_todo_flutter_2022/features/tasks/presentation/tasks_list_item.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
+
+import '../../../test_utilities/fixtures/task_fixture.dart';
 
 void main() {
   group('GIVEN TasksList', () {
-    group('WHEN pumped ', () {
-      // testWidgets(
-      //   'THEN displays how many tasks are done today',
-      //   (tester) async {
-      //     await tester.pumpWithDependencies(const TasksList());
+    testWidgets(
+      'WHEN there are no tasks '
+      'THEN displays nothing',
+      (tester) async {
+        final cubit = TasksCubit()..update([]);
 
-      //     expect(find.text('Wins today: 0'), findsOneWidget);
-      //   },
-      // );
+        await tester.pumpWithDependencies(
+          tasksCubit: cubit,
+          child: const TasksList(),
+        );
+
+        expect(find.byType(TasksListItem), findsNothing);
+      },
+    );
+
+    group('WHEN there are two tasks', () {
+      testWidgets(
+        'THEN displays 2 TasksListItem',
+        (tester) async {
+          final cubit = TasksCubit()..update([taskFixture, taskFixture]);
+
+          await tester.pumpWithDependencies(
+            tasksCubit: cubit,
+            child: const TasksList(),
+          );
+
+          expect(find.byType(TasksListItem), findsNWidgets(2));
+        },
+      );
     });
   });
 }
 
-extension _PumpWithScaffold on WidgetTester {
-  Future<void> pumpWithDependencies(Widget child) {
+extension on WidgetTester {
+  Future<void> pumpWithDependencies({
+    required Widget child,
+    required TasksCubit tasksCubit,
+  }) {
     // ignore: unnecessary_this
     return this.pumpWidget(
-      Directionality(
-        textDirection: TextDirection.ltr,
-        child: child,
+      BlocProvider(
+        create: (context) => tasksCubit,
+        child: Directionality(
+          textDirection: TextDirection.ltr,
+          child: child,
+        ),
       ),
     );
   }
