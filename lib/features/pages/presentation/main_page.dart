afbb12224dbd779898cefe0066ccc4531ecba857
Mikhail Palei <mikhail_palei@epam.com>
2022-06-05 20:11:48 +0500

    feat: adds MainPage to main

--- lib/features/pages/presentation/main_page.dart
+++ lib/features/pages/presentation/main_page.dart
@@ -1,11 +1,39 @@
+import 'package:flow_todo_flutter_2022/features/tasks/domain/task.dart';
+import 'package:flow_todo_flutter_2022/features/tasks/presentation/cubit/tasks_cubit.dart';
 import 'package:flow_todo_flutter_2022/features/tasks/presentation/tasks_list.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class MainPage extends StatelessWidget {
+class MainPage extends StatefulWidget {
   const MainPage({Key? key}) : super(key: key);
 
   @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  final _tasksCubit = TasksCubit();
+
+  final _taskFixture = Task(
+    id: 'id',
+    title: 'title',
+    dueAt: 00,
+    isDone: false,
+    userId: 'userId',
+    createdAt: 0,
+  );
+
+  @override
+  void initState() {
+    super.initState();
+    _tasksCubit.update([_taskFixture, _taskFixture]);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const TasksList();
+    return BlocProvider(
+      child: const TasksList(),
+      create: (context) => _tasksCubit,
+    );
   }
 }
