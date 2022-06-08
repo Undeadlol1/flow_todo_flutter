68b303f13156418b9d9eb59b779f30658098e76e
Mikhail Palei <mikhail_palei@epam.com>
2022-06-08 23:04:43 +0500

    feat: adds UpdateTaskRepository to main

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
 import 'package:flow_todo_flutter_2022/features/authentification/presentation/cubit/authentification_cubit.dart';
 import 'package:flow_todo_flutter_2022/features/tasks/data/get_tasks_to_do_repository.dart';
+import 'package:flow_todo_flutter_2022/features/tasks/data/update_task_repository.dart';
 import 'package:flow_todo_flutter_2022/features/tasks/domain/use_cases/get_tasks_to_do.dart';
 import 'package:flow_todo_flutter_2022/features/tasks/domain/use_cases/go_to_task_page.dart';
 import 'package:flow_todo_flutter_2022/features/tasks/presentation/pages/task_page.dart';
@@ -46,6 +47,7 @@ _setUpDI() {
   injector.registerSingleton(FirebaseFirestore.instance);
   injector.registerSingleton(const GetTasksToDo());
   injector.registerSingleton(GetTasksToDoRepository(firestore: injector.get()));
+  injector.registerSingleton(UpdateTaskRepository(firestore: injector.get()));
   injector.registerSingleton(GoToTaskPage(contextProvider: injector.get()));
   injector.registerSingleton(_tasksCubit);
 }
