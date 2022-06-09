ed12b60c9a45eef55147ca48b16f2ad7b7011315
Mikhail Palei <mikhail_palei@epam.com>
2022-06-09 13:57:54 +0500

    feat: improves pagination widget

--- lib/features/common/presentation/widgets/pagination.dart
+++ lib/features/common/presentation/widgets/pagination.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 
-class Pagination extends StatelessWidget {
-  const Pagination({Key? key}) : super(key: key);
+class Pagination extends StatefulWidget {
+  final void Function(int newPageNumber) onPageChange;
+  const Pagination({Key? key, required this.onPageChange}) : super(key: key);
+
+  @override
+  State<Pagination> createState() => _PaginationState();
+}
+
+class _PaginationState extends State<Pagination> {
+  int _currentPage = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -9,11 +17,24 @@ class Pagination extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         IconButton(
-          onPressed: () {},
+          onPressed: () {
+            --_currentPage;
+
+            if (_currentPage.isNegative) {
+              _currentPage = 0;
+            }
+
+            widget.onPageChange(_currentPage);
+            setState(() => _currentPage = _currentPage);
+          },
           icon: const Icon(Icons.arrow_back_ios),
         ),
         IconButton(
-          onPressed: () {},
+          onPressed: () {
+            ++_currentPage;
+            widget.onPageChange(_currentPage);
+            setState(() => _currentPage = _currentPage);
+          },
           icon: const Icon(Icons.arrow_forward_ios),
         ),
       ],
