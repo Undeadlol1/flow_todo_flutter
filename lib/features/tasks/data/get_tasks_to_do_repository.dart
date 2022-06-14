86f48227868494958d4f0ff86afc7d6e660b2c94
Mihail Paley <undeadlol1@mail.ru>
2022-06-14 10:09:41 +0500

    feat: parse json via isolate in GetTasksRepository

--- lib/features/tasks/data/get_tasks_to_do_repository.dart
+++ lib/features/tasks/data/get_tasks_to_do_repository.dart
@@ -1,20 +1,29 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flow_todo_flutter_2022/features/tasks/domain/models/task.dart';
+import 'package:flutter/foundation.dart';
 
 class GetTasksToDoRepository {
   final FirebaseFirestore firestore;
   const GetTasksToDoRepository({required this.firestore});
 
   Future<List<Task>> call({required String userId}) async {
-    final tasks = await firestore
+    final tasksSnapshot = await firestore
         .collection('tasks')
         .where('userId', isEqualTo: userId)
         .where('isDone', isEqualTo: false)
-        .where('dueAt',
-            isLessThanOrEqualTo: DateTime.now().millisecondsSinceEpoch)
+        .where(
+          'dueAt',
+          isLessThanOrEqualTo: DateTime.now().millisecondsSinceEpoch,
+        )
         .limit(100)
         .get();
 
-    return tasks.docs.map((e) => Task.fromJson(e.data())).toList();
+    final listOfJsons = tasksSnapshot.docs.map((e) => e.data()).toList();
+
+    return compute(_parseListOfJsons, listOfJsons);
   }
+}
+
+List<Task> _parseListOfJsons(List<Map<String, dynamic>> docs) {
+  return docs.map(Task.fromJson).toList();
 }
