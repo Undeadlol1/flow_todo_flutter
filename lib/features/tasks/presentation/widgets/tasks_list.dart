32457d29300078c9877690807c6d6ca178f9f190
Mikhail Palei <mikhail_palei@epam.com>
2022-06-22 00:42:30 +0500

    fix: remove AnimatedList from TasksList because of too much maintnance

--- lib/features/tasks/presentation/widgets/tasks_list.dart
+++ lib/features/tasks/presentation/widgets/tasks_list.dart
@@ -1,58 +1,31 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutterfire_ui/auth.dart';
-import 'package:get_it/get_it.dart';
 
 import '../../../common/presentation/widgets/pagination.dart';
-import '../../domain/models/task.dart';
 import '../cubit/tasks_cubit.dart';
 import 'tasks_list_item.dart';
 
-class TasksList extends StatefulWidget {
+class TasksList extends StatelessWidget {
   const TasksList({Key? key}) : super(key: key);
 
   @override
-  State<TasksList> createState() => _TasksListState();
-}
-
-class _TasksListState extends State<TasksList> with TickerProviderStateMixin {
-  List<Task> _localTasksList = GetIt.I<TasksCubit>().state.tasks;
-
-  final _animatedListKey = GlobalKey<AnimatedListState>();
-  final _offsetAnimationTween = Tween<Offset>(
-    begin: const Offset(0, 10),
-    end: const Offset(0.0, 0),
-  );
-
-  @override
-  void initState() {
-    Future.microtask(_ensureTasksAreTakenFromCubitWhenWidgetLoaded);
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return BlocConsumer<TasksCubit, TasksState>(
-      listener: _syncAnimatedListWithCubitsList,
+    return BlocBuilder<TasksCubit, TasksState>(
       builder: (context, tasksState) {
         if (tasksState is TasksLoading) {
           return const _LoadingIndicator();
         }
         return Column(
           children: [
-            AnimatedList(
+            ListView.builder(
               shrinkWrap: true,
-              key: _animatedListKey,
-              initialItemCount: _localTasksList.length,
               physics: const NeverScrollableScrollPhysics(),
-              itemBuilder: (_, index, animation) {
-                return SlideTransition(
-                  position: animation.drive(_offsetAnimationTween),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: TasksListItem(task: _localTasksList[index]),
-                  ),
+              itemCount: tasksState.tasks.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TasksListItem(task: tasksState.tasks[index]),
                 );
               },
             ),
@@ -64,37 +37,6 @@ class _TasksListState extends State<TasksList> with TickerProviderStateMixin {
         );
       },
     );
-  }
-
-  void _syncAnimatedListWithCubitsList(_, TasksState tasksState) {
-    final animatedList = _animatedListKey.currentState;
-
-    for (final task in tasksState.tasks) {
-      if (!_localTasksList.contains(task)) {
-        final taskIndex = tasksState.tasks.indexOf(task);
-
-        _localTasksList.insert(taskIndex, task);
-        animatedList?.insertItem(taskIndex);
-      }
-    }
-
-    for (final task in _localTasksList) {
-      if (!tasksState.tasks.contains(task)) {
-        final taskIndex = _localTasksList.indexOf(task);
-
-        _localTasksList.remove(task);
-        animatedList?.removeItem(taskIndex, (_, __) => Container());
-      }
-
-      setState(() => _localTasksList = _localTasksList);
-    }
-  }
-
-  FutureOr<void> _ensureTasksAreTakenFromCubitWhenWidgetLoaded() {
-    setState(() {
-      _localTasksList =
-          BlocProvider.of<TasksCubit>(context, listen: false).state.tasks;
-    });
   }
 }
 
