08384f1539c071705c7ec6fc3446b0f589682ea5
Mikhail Palei <mikhail_palei@epam.com>
2022-06-22 03:48:46 +0500

    fix: :mute: remove a log

--- test/features/tasks/domain/models/task_test.dart
+++ test/features/tasks/domain/models/task_test.dart
@@ -1,6 +1,3 @@
-import 'package:flow_todo_flutter_2022/features/tasks/domain/entities/task_history_action_type.dart';
-import 'package:flow_todo_flutter_2022/features/tasks/domain/models/task_history.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import '../../../../test_utilities/fixtures/task_fixture.dart';
@@ -16,9 +13,6 @@ void main() {
         () {
           final result =
               taskFixture.copyWith(history: [taskHistoryFixture]).toJson();
-
-          debugPrint('result $result');
-          // debugPrint('result.history $result.hi');
 
           expect(result['history'], hasLength(1));
           expect(result['history'][0], isA<Map>());
