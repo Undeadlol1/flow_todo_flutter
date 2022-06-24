25d4a9ac34ef1d26c6fb63201af5ff682570b410
Mikhail Palei <mikhail_palei@epam.com>
2022-06-25 03:54:35 +0500

    fix: remove development only code

--- lib/features/tasks/presentation/widgets/tasks_list_item.dart
+++ lib/features/tasks/presentation/widgets/tasks_list_item.dart
@@ -1,7 +1,8 @@
 import 'package:flow_todo_flutter_2022/features/tasks/domain/models/task.dart';
-import 'package:flow_todo_flutter_2022/features/users/presentation/cubit/profile_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
+
+import '../../domain/use_cases/go_to_task_page.dart';
 
 class TasksListItem extends StatelessWidget {
   final Task task;
@@ -11,15 +12,7 @@ class TasksListItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return TextButton(
       onPressed: () {
-        final cubit = GetIt.I<ProfileCubit>();
-        final profile = cubit.state.profile;
-        cubit.setProfile(
-          profile!.copyWith(
-            points: profile.points + 50,
-            experience: profile.experience! + 50,
-          ),
-        );
-        // GetIt.instance<GoToTaskPage>()(task: task);
+        GetIt.instance<GoToTaskPage>()(task: task);
       },
       child: Row(
         children: [
