40e9b1faaadeb6ad220bcc76fe967c4d690e49db
Mikhail Palei <mikhail_palei@epam.com>
2022-07-13 21:39:54 +0500

    feat: Countdown complete

--- lib/features/common/presentation/widgets/countdown.dart
+++ lib/features/common/presentation/widgets/countdown.dart
@@ -10,8 +10,10 @@ class CountDown extends StatefulWidget {
 }
 
 class _CountDownState extends State<CountDown> {
-  late final Stopwatch stopwatch;
   late final Timer timer;
+  late final Stopwatch stopwatch;
+
+  final _countdownDuration = const Duration(minutes: 5);
 
   @override
   void initState() {
@@ -32,11 +34,19 @@ class _CountDownState extends State<CountDown> {
 
   @override
   Widget build(BuildContext context) {
-    const countdownDuration = Duration(seconds: 5);
-    final timeToDisplay =
-        countdownDuration.inSeconds - stopwatch.elapsed.inSeconds;
     final isCountdownCompleted =
-        stopwatch.elapsed.inMilliseconds > countdownDuration.inMilliseconds;
+        stopwatch.elapsed.inMilliseconds > _countdownDuration.inMilliseconds;
+    final timeToDisplay =
+        (_countdownDuration.inMinutes - stopwatch.elapsed.inMinutes) - 1;
+    final minutesRemaining = timeToDisplay - stopwatch.elapsed.inMinutes < 0
+        ? 0
+        : timeToDisplay - stopwatch.elapsed.inMinutes;
+    final secondsRemaining =
+        (_countdownDuration.inSeconds - stopwatch.elapsed.inSeconds) -
+            (minutesRemaining * 60) -
+            1;
+
+    debugPrint('${stopwatch.elapsed}');
 
     if (isCountdownCompleted) {
       timer.cancel();
@@ -44,9 +54,12 @@ class _CountDownState extends State<CountDown> {
 
     return FloatingActionButton(
       onPressed: null,
-      child: isCountdownCompleted
-          ? const Icon(Icons.check)
-          : Text(timeToDisplay.toString()),
+      child: AnimatedSwitcher(
+        duration: const Duration(milliseconds: 400),
+        child: isCountdownCompleted
+            ? const Icon(Icons.check)
+            : Text('$minutesRemaining:$secondsRemaining'),
+      ),
     );
   }
 }
