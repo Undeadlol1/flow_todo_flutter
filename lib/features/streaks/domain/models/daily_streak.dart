76a74ddb2c6a3a48e760d0a014aa0229197e590b
Mikhail Palei <mikhail_palei@epam.com>
2022-07-21 16:20:17 +0500

    fix: clean up

--- lib/features/streaks/domain/models/daily_streak.dart
+++ lib/features/streaks/domain/models/daily_streak.dart
@@ -39,7 +39,6 @@ class DailyStreak with _$DailyStreak {
   }
 
   bool shouldStreakIncrement({required final int tasksDoneToday}) {
-    print('was updated today: ${_wasStreakUpdatedToday()}');
     final bool isTaskGoalReached = tasksDoneToday >= perDay;
 
     if (updatedAt == null && isTaskGoalReached) {
@@ -56,40 +55,11 @@ class DailyStreak with _$DailyStreak {
   bool _wasStreakUpdatedToday() {
     final today = DateTime.now();
 
-    final bool wasStreakStartedYesterday =
-        DateTime.fromMillisecondsSinceEpoch(startsAt)
-                .difference(DateTime.now())
-                .inDays ==
-            1;
-
-    final updatedAgo = today.difference(
-      DateTime.fromMillisecondsSinceEpoch(
-        updatedAt ?? today.millisecondsSinceEpoch,
-      ),
-    );
-
-    final wasStreakUpdatedYesterday = updatedAgo.inDays <= 1 ? true : false;
-
-    print('-----------------------');
-    print('today: ${today.toString()}');
-    print(
-      'updatedAt: ${updatedAt != null ? DateTime.fromMillisecondsSinceEpoch(updatedAt!) : 'is null'}',
-    );
-    print('updatedAgo: ${updatedAgo.toString()}');
-    print('updatedAgo.inDays: ${updatedAgo.inDays.toString()}');
-    print('wasStreakUpdatedYesterday $wasStreakUpdatedYesterday');
-
     return updatedAt == null
         ? false
         : today
                 .difference(DateTime.fromMillisecondsSinceEpoch(updatedAt!))
                 .inDays ==
             0;
-
-    // if (updatedAt == null && wasStreakStartedYesterday) {
-    //   return true;
-    // }
-
-    // return updatedAgo.inDays > 1;
   }
 }
