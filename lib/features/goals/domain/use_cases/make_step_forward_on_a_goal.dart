34df90630b6bb688c52dfb337203e2af5f519e34
Mikhail Palei <mikhail_palei@epam.com>
2022-07-22 05:22:53 +0500

    feat: debounce MakeStepForwardOnGoal

--- lib/features/goals/domain/use_cases/make_step_forward_on_a_goal.dart
+++ lib/features/goals/domain/use_cases/make_step_forward_on_a_goal.dart
@@ -1,25 +1,48 @@
+import 'dart:developer';
+
 import 'package:flow_todo_flutter_2022/features/goals/data/update_goal_repository.dart';
 import 'package:flow_todo_flutter_2022/features/goals/domain/models/goal.dart';
 import 'package:flow_todo_flutter_2022/features/goals/presentation/cubit/goals_cubit.dart';
 import 'package:injectable/injectable.dart';
+import 'package:easy_debounce/easy_debounce.dart';
 
 @lazySingleton
 class MakeStepForwardOnAGoal {
   final GoalsCubit goalsCubit;
   final UpdateGoalRepository updateGoalRepo;
-  const MakeStepForwardOnAGoal({
+  MakeStepForwardOnAGoal({
     required this.goalsCubit,
     required this.updateGoalRepo,
   });
 
+  int _timesUseCaseCalled = 0;
+
   Future<void> call(Goal goal) async {
+    log('goal: ${goal.toString()}');
     final updatedGoal = goal.copyWith(
-      steps: goal.steps + 1,
-      points: goal.points + 10,
+      steps: 1 + goal.steps,
+      points: 10 + goal.points,
       updatedAt: DateTime.now().millisecondsSinceEpoch,
     );
 
     goalsCubit.updateGoal(updatedGoal);
+
+    return _debounceRepositoryCall(updatedGoal);
+  }
+
+  void _debounceRepositoryCall(Goal goal) {
+    _timesUseCaseCalled++;
+    EasyDebounce.debounce(
+      'goals-update-debouncle-id',
+      const Duration(milliseconds: 1500),
+      () => callRepository(goal),
+    );
+  }
+
+  Future<void> callRepository(Goal updatedGoal) {
+    log('Actual  function call');
+    log('amount called $_timesUseCaseCalled');
+    _timesUseCaseCalled = 0;
     return updateGoalRepo(updatedGoal);
   }
 }
