1e67e97d560eeeb7572b9a7c91f9c53db00aba73
Mikhail Palei <mikhail_palei@epam.com>
2022-07-28 02:26:28 +0500

    fix: dependency not registered in DI

--- lib/features/tasks/presentation/cubit/filtered_tasks_cubit.dart
+++ lib/features/tasks/presentation/cubit/filtered_tasks_cubit.dart
@@ -1,11 +1,13 @@
 import 'package:bloc/bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:injectable/injectable.dart';
 
 import '../../domain/models/task.dart';
 
 part 'filtered_tasks_state.dart';
 part 'filtered_tasks_cubit.freezed.dart';
 
+@injectable
 class FilteredTasksCubit extends Cubit<FilteredTasksState> {
   FilteredTasksCubit() : super(FilteredTasksState([]));
 
