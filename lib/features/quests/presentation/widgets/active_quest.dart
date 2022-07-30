5c5664b7e97a7d20595ca50a075a94292e02ff01
Mihail Paley <undeadlol1@mail.ru>
2022-07-30 06:14:29 +0500

    feat: :construction: display active quest to viewer WIP

--- lib/features/quests/presentation/widgets/active_quest.dart
+++ lib/features/quests/presentation/widgets/active_quest.dart
@@ -1,3 +1,4 @@
+import 'package:flow_todo_flutter_2022/features/quests/presentation/cubits/active_quests_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
@@ -16,30 +17,36 @@ class ActiveQuest extends StatelessWidget {
   );
 
   @override
-  Widget build(BuildContext context) {
-    return BlocBuilder<ProfileCubit, ProfileState>(
-      builder: (context, profileState) {
-        if (profileState is ProfileLoaded) {
-          final level =
-              _levelCalculator(profileState.profile?.experience ?? 0).value;
+  Widget build(context) {
+    return Builder(
+      builder: (context) {
+        final profileState = context.watch<ProfileCubit>().state;
+        final questsState = context.watch<ActiveQuestsCubit>().state;
+        return questsState.maybeMap(
+          orElse: _getLoadingWidget,
+          loaded: (_) {
+            final level =
+                _levelCalculator(profileState.profile?.experience ?? 0).value;
 
-          return Padding(
-            padding: _padding,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const SizedBox(height: 15),
-                Text('Quest: reach level ${level + 5}'),
-                const _ProgressBar(),
-              ],
-            ),
-          );
-        } else {
-          return const SizedBox();
-        }
+            return Padding(
+              padding: _padding,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 15),
+                  Text('Quest: ${questsState.quests.first.type.name}'),
+                  Text('Quest: reach level ${level + 5}'),
+                  const _ProgressBar(),
+                ],
+              ),
+            );
+          },
+        );
       },
     );
   }
+
+  Widget _getLoadingWidget() => const SizedBox();
 }
 
 class _ProgressBar extends StatelessWidget {
