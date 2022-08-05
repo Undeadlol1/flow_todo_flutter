08d0f41dad82643b240407143b9d2e230f421b50
Mikhail Palei <mikhail_palei@epam.com>
2022-08-05 07:54:22 +0500

    feat: improve tasks caching and load time

--- lib/features/tasks/domain/use_cases/get_tasks_to_do.dart
+++ lib/features/tasks/domain/use_cases/get_tasks_to_do.dart
@@ -2,6 +2,8 @@ import 'package:flow_todo_flutter_2022/features/tasks/data/get_tasks_to_do_repos
 import 'package:flow_todo_flutter_2022/features/tasks/presentation/cubit/tasks_cubit.dart';
 import 'package:injectable/injectable.dart';
 
+import '../models/task.dart';
+
 @singleton
 class GetTasksToDo {
   final TasksCubit tasksCubit;
@@ -9,11 +11,42 @@ class GetTasksToDo {
   GetTasksToDo({required this.getTasks, required this.tasksCubit});
 
   Future<void> call({required String userId}) async {
-    tasksCubit.setLoading();
+    if (tasksCubit.state is! TasksUpdated) {
+      tasksCubit.setLoading();
+    }
 
-    final tasks = await getTasks(userId: userId)
-      ..shuffle();
+    final remoteTasks = await getTasks(userId: userId);
 
-    tasksCubit.updateList(tasks);
+    _updateStateIfNecessary(remoteTasks);
+  }
+
+  void _updateStateIfNecessary(List<Task> remoteTasks) {
+    if (_cachedTasksAreNotEqualToRemotetasks(
+      remoteTasks: remoteTasks,
+      localTasks: tasksCubit.state.tasks,
+    )) {
+      remoteTasks.shuffle();
+      tasksCubit.updateList(remoteTasks);
+    }
+  }
+
+  bool _cachedTasksAreNotEqualToRemotetasks({
+    required List<Task> remoteTasks,
+    required List<Task> localTasks,
+  }) {
+    if (remoteTasks.length != localTasks.length) {
+      return true;
+    }
+
+    final areLocalTasksNotEqualToRemote = remoteTasks.any((remoteTask) {
+      try {
+        final localTask = localTasks.firstWhere((i) => i.id == remoteTask.id);
+        return localTask != remoteTask;
+      } catch (e) {
+        return true;
+      }
+    });
+
+    return areLocalTasksNotEqualToRemote;
   }
 }
