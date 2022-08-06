c74c5f59808eed3cc2eba169ce5220f0755270b1
Mikhail Palei <mikhail_palei@epam.com>
2022-08-06 06:36:13 +0500

    chore: clean up

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,6 @@ void main() async {
 
       runApp(const App());
     },
-    // blocObserver: _GlobalBlocObserver(),
     createStorage: () async {
       return HydratedStorage.build(
         storageDirectory: kIsWeb
@@ -58,37 +57,3 @@ Future<void> _configureFirebase() async {
     ),
   ]);
 }
-
-// class _GlobalBlocObserver extends BlocObserver {
-//   @override
-//   void onCreate(BlocBase bloc) {
-//     super.onCreate(bloc);
-//     if (kDebugMode) {
-//       print('onCreate -- ${bloc.runtimeType}');
-//     }
-//   }
-
-//   @override
-//   void onChange(BlocBase bloc, Change change) {
-//     super.onChange(bloc, change);
-//     if (kDebugMode) {
-//       print('onChange -- ${bloc.runtimeType}, $change');
-//     }
-//   }
-
-//   @override
-//   void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
-//     if (kDebugMode) {
-//       print('onError -- ${bloc.runtimeType}, $error');
-//     }
-//     super.onError(bloc, error, stackTrace);
-//   }
-
-//   @override
-//   void onClose(BlocBase bloc) {
-//     super.onClose(bloc);
-//     if (kDebugMode) {
-//       print('onClose -- ${bloc.runtimeType}');
-//     }
-//   }
-// }
