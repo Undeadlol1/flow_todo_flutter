469ee7d31698a515649154ff54492b5b42586525
Mikhail Palei <paleyblog@gmail.com>
2022-08-28 02:31:39 +0500

    feat: track stale task being navigated to

--- test/features/tasks/domain/use_cases/go_to_task_page_test.dart
+++ test/features/tasks/domain/use_cases/go_to_task_page_test.dart
@@ -1,12 +1,22 @@
+import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:flow_todo_flutter_2022/features/tasks/domain/use_cases/go_to_task_page.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
 import '../../../../test_utilities/fixtures/task_fixture.dart';
 import '../../../../test_utilities/mocks/mock_build_context_provider.dart';
+import '../../../../test_utilities/mocks/mock_stale_task_detector.dart';
+
+class _MockFirebaseAnalytics extends Mock implements FirebaseAnalytics {}
 
 void main() {
   final buildContextProvider = MockBuildContextProvider();
+  final MockStaleTaskDetector mockStaleTaskDetector = MockStaleTaskDetector();
 
-  tearDown(buildContextProvider.reset);
+  setUpAll(() {
+    registerFallbackValue(taskFixture);
+
+    when(() => mockStaleTaskDetector.isStale(any())).thenReturn(false);
+  });
 
   group(
     'GIVEN GoToTaskPage '
@@ -16,7 +26,11 @@ void main() {
       test(
         "redirects to task page",
         () {
-          GoToTaskPage(provideContext: buildContextProvider)(
+          GoToTaskPage(
+            provideContext: buildContextProvider,
+            staleTaskDetector: mockStaleTaskDetector,
+            firebaseAnalytics: _MockFirebaseAnalytics(),
+          )(
             task: taskFixture,
           );
 
