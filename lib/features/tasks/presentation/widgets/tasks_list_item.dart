0a6fce9ce104b9ee9e6275b841cc805c1cb2eafc
Mikhail Palei <paleyblog@gmail.com>
2022-08-27 23:36:16 +0500

    feat: swipe TaskListItem to complete it

--- lib/features/tasks/presentation/widgets/tasks_list_item.dart
+++ lib/features/tasks/presentation/widgets/tasks_list_item.dart
@@ -1,5 +1,7 @@
+import 'package:flow_todo_flutter_2022/features/spaced_repetition/domain/entities/confidence.dart';
 import 'package:flow_todo_flutter_2022/features/tasks/domain/models/task.dart';
 import 'package:flow_todo_flutter_2022/features/tasks/domain/services/task_reward_calculator.dart';
+import 'package:flow_todo_flutter_2022/features/tasks/domain/use_cases/make_step_forward_on_the_task.dart';
 import 'package:flow_todo_flutter_2022/features/tasks/domain/use_cases/toggle_task_selection.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
@@ -13,16 +15,46 @@ class TasksListItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final TaskRewardCalculator rewardCalculator = GetIt.I();
-    return ListTile(
-      dense: false,
-      enableFeedback: true,
-      title: Text(task.title),
-      selected: task.isSelected,
-      subtitle: Text(
-        'Reward: ${rewardCalculator(task)} experience',
+    return Dismissible(
+      key: ValueKey<String>(task.id),
+      direction: DismissDirection.startToEnd,
+      background: const _DismissibleBackground(),
+      onDismissed: (DismissDirection direction) {
+        GetIt.I<MakeStepForwardOnTheTask>()(
+          task: task,
+          isTaskDone: true,
+          howBigWasTheStep: Confidence.good,
+        );
+      },
+      child: ListTile(
+        dense: false,
+        enableFeedback: true,
+        title: Text(task.title),
+        selected: task.isSelected,
+        subtitle: Text(
+          'Reward: ${rewardCalculator(task)} experience',
+        ),
+        onTap: () => GetIt.I<GoToTaskPage>()(task: task),
+        onLongPress: () => GetIt.I<ToggleTaskSelection>()(task),
       ),
-      onTap: () => GetIt.I<GoToTaskPage>()(task: task),
-      onLongPress: () => GetIt.I<ToggleTaskSelection>()(task),
+    );
+  }
+}
+
+class _DismissibleBackground extends StatelessWidget {
+  const _DismissibleBackground({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Container(
+      padding: const EdgeInsets.all(20),
+      alignment: Alignment.centerLeft,
+      color: theme.focusColor,
+      child: Text(
+        'Complete the task',
+        style: theme.textTheme.subtitle2?.copyWith(fontStyle: FontStyle.italic),
+      ),
     );
   }
 }
