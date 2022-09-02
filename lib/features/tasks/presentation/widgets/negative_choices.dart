98019df5175a7ae4201d44f62c1f4b8d9d4edb99
Mikhail Palei <paleyblog@gmail.com>
2022-09-03 02:24:36 +0500

    feat: slight text chagne

--- lib/features/tasks/presentation/widgets/negative_choices.dart
+++ lib/features/tasks/presentation/widgets/negative_choices.dart
@@ -21,7 +21,7 @@ class NegativeChoices extends StatelessWidget {
         ),
         ListTile(
           leading: const Icon(Icons.title),
-          title: _buildText('Rename the task'),
+          title: _buildText('Rework the task'),
           onTap: () => _navigateToTaskPage(
             context: context,
             isTitleEditingVisible: true,
