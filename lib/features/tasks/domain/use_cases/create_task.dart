c317ba4a8c81764c5c8465c9c18b2cbd91e7416a
Mikhail Palei <paleyblog@gmail.com>
2022-09-04 20:26:02 +0500

    feat: add exception handler to CreateTask

--- lib/features/tasks/domain/use_cases/create_task.dart
+++ lib/features/tasks/domain/use_cases/create_task.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:flow_todo_flutter_2022/core/services/use_case_exception_handler.dart';
 import 'package:flow_todo_flutter_2022/features/common/services/get_todays_date.dart';
 import 'package:flow_todo_flutter_2022/features/common/services/unique_id_generator.dart';
 import 'package:flow_todo_flutter_2022/features/tasks/data/create_task_repository.dart';
@@ -17,6 +18,7 @@ class CreateTask {
   final FirebaseAnalytics firebaseAnalytics;
   final UniqueIdGenerator uniqueIdGenerator;
   final CreateTaskRepository createTaskRepository;
+  final UseCaseExceptionHandler useCaseExceptionHandler;
   CreateTask({
     required this.tasksCubit,
     required this.profileCubit,
@@ -25,6 +27,7 @@ class CreateTask {
     required this.firebaseAnalytics,
     required this.uniqueIdGenerator,
     required this.createTaskRepository,
+    required this.useCaseExceptionHandler,
   });
 
   Future<void> call({
@@ -32,26 +35,31 @@ class CreateTask {
     required String userId,
     required List<String> tags,
   }) async {
-    final profileState = profileCubit.state;
-    final taskToCreate = Task(
-      note: '',
-      tags: tags,
-      history: [],
-      isDone: false,
-      userId: userId,
-      title: title.trim(),
-      id: uniqueIdGenerator(),
-      dueAt: getTodaysDate(),
-      createdAt: getTodaysDate(),
-    );
+    try {
+      final profileState = profileCubit.state;
+      final taskToCreate = Task(
+        note: '',
+        tags: tags,
+        history: [],
+        isDone: false,
+        userId: userId,
+        title: title.trim(),
+        id: uniqueIdGenerator(),
+        dueAt: getTodaysDate(),
+        createdAt: getTodaysDate(),
+      );
 
-    if (profileState is ProfileLoaded) {
-      addPointsToUser(10);
+      if (profileState is ProfileLoaded) {
+        addPointsToUser(10);
+      }
+      tasksCubit
+          .updateList([...tasksCubit.state.tasks]..insert(0, taskToCreate));
+
+      await createTaskRepository(taskToCreate);
+
+      return firebaseAnalytics.logEvent(name: 'create_task');
+    } catch (error, stack) {
+      useCaseExceptionHandler(error, stack);
     }
-    tasksCubit.updateList([...tasksCubit.state.tasks]..insert(0, taskToCreate));
-
-    await createTaskRepository(taskToCreate);
-
-    return firebaseAnalytics.logEvent(name: 'create_task');
   }
 }
